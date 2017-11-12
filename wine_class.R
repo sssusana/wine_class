5162c97d70ab5aadaf425a916f62faad93ca5b68
sssusana <siss.susana@gmail.com>
2017-11-12 02:27:42 +0000

    [UPD]
    Added new model
    minsplit = 10, cp = 0.002)

--- wine_class.R
+++ wine_class.R
@@ -5,6 +5,7 @@ library(rpart.plot)
 library(caret)
 library(party)
 library(partykit)
+
 #Load 2k DB:
 exerc1 <- read_excel("~/Documents/GitHub/wine_class/Exer1.xls")
 #Load 8k DB:
@@ -35,49 +36,67 @@ print(s, exclude1 = TRUE)
 #Decision Tree with rpart
 #Method = "class" if you have a classification tree
 #Method = "anova" if you have a regression tree
-#Variables actually used in tree construction: Dayswus  Income   Monetary Recency 
-colnames(dt_wines)
-dtwines <-rpart(formula = Spcork ~ Dayswus + Income + Recency + Monetary + Age + Edu + Teenhome + Kidhome + Freq, data = dt_wines, method = "class")
+
+colnames(exerc1)
+dtwines <-rpart(formula = Spcork ~ Dayswus + Income + Recency + Monetary + Age + Edu + Teenhome + Kidhome + Freq, data = exerc1, method = "class")
 printcp(dtwines)
 as.party(dtwines) 
-print("Now we see how many variables don't really matter")
+#Now we see how many variables don't really matter")
+#Variables actually used in tree construction: Dayswus  Income   Monetary Recency
 
-#Only with the proper var + rpart.control
-dtwine_control <- rpart(Spcork ~ 
+
+#Run the model only with the proper var + rpart.control
+dtwine_reduced <- rpart(Spcork ~ 
                    Dayswus + Income + Recency + Monetary, 
-                   data = dt_wines, method="class", control = rpart.control(minsplit = 40, cp = 0.005))
+                   data = exerc1, method="class", control = rpart.control(minsplit = 40, cp = 0.005))
 
 #Info + viz on Decision Tree
-as.party(dtwine_control)
-printcp(dtwine_control)
-rpart.plot(dtwine_control)
+as.party(dtwine_reduced)
+printcp(dtwine_reduced)
+rpart.plot(dtwine_reduced)
 
 
 #Making Predictions
-myprediction <- predict(dtwine_control, newdata = customersDB, type="class")
+myprediction <- predict(dtwine_reduced, newdata = customersDB, type="class")
 plot(myprediction)
-mysolution <- data.frame(Custid=customersDB$Custid, Spcork = myprediction)
-solution_merged <- merge(customersDB, mysolution, by="Custid")
+mysolution <- data.frame(customersDB, Spcork = myprediction)
+mysolution
+
 
 #Confusing Matrix files (testing model in the 2k)
-dtwines2k <-dt_wines
-predkfold <- predict(dtwine_control, newdata = dtwines2k, type="class")
-predkfold_sol <- data.frame(Custid=dt_wines$Custid, kfold=predkfold) 
-predkfold_merged <- merge(dt_wines, predkfold_sol, by="Custid")
+dtwines2k <- exerc1
+pred_test <- predict(dtwine_reduced, newdata = dtwines2k, type="class")
+pred_test_ds <- data.frame(ID=dtwines2k$Custid, Spcork=dtwines2k$Spcork, Pred=pred_test) 
 
+#Testing Da
 #ConfusionMatrix for 2k
-confusionMatrix2k <- confusionMatrix(predkfold_merged$kfold, predkfold_merged$Spcork)
+confusionMatrix2k <- confusionMatrix(pred_test_ds$Spcork, pred_test_ds$Pred)
 confusionMatrix2k
 
-#Saving
-write.table(solution_merged, file= "mysm.xls", col.names = TRUE)
 
-dt_solution <- read_excel("~/Documents/GitHub/wine_class/mysm.xls")
+############################################Model 2
+
+model2 <- rpart(Spcork ~ 
+                          Dayswus + Income + Recency + Monetary, 
+                        data = exerc1, method="class", control = rpart.control(minsplit = 10, cp = 0.002))
+#Info + viz on Decision Tree
+as.party(model2)
+printcp(model2)
+#PRED
+dtwines_model2 <- exerc1
+pred_model2 <- predict(model2, newdata = dtwines_model2, type="class")
+pred_model2_ds <- data.frame(ID=dtwines_model2$Custid, Spcork=dtwines_model2$Spcork, Pred=pred_model2) 
+#Test
+confusionMatrix_model2 <- confusionMatrix(pred_model2_ds$Spcork, pred_model2_ds$Pred)
+confusionMatrix_model2
+
 
 #Decision Tree on Predicted Data aka.. Testing solution
+#Load 8k table with out predictions
+
 prop.table(table(mysm$Spcork))
 
-testsolution<- rpart(formula = Spcork ~ Dayswus + Income + Recency + Monetary, data = mysm, 
+testsolution<- rpart(formula = Spcork ~ Dayswus + Income + Recency + Monetary, data = solution_merged, 
                      method = "class",
                      control = rpart.control(minsplit = 40, cp = 0))
 printcp(testsolution)
