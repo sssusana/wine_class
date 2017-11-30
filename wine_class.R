fe16fb3ca9d6809971a88dd62710611a675400c0
sssusana <siss.susana@gmail.com>
2017-11-30 16:56:11 +0000

    [UPD]
    train+testset

--- wine_class.R
+++ wine_class.R
@@ -26,13 +26,15 @@ prop.table(table(exerc1$Spcork,exerc1$Teenhome))
 
 #Dataset only with buyers
 buyers <- exerc1[which(exerc1$Spcork == 1), ]
-
+notbuyers <- exerc1[which(exerc1$Spcork == 0), ]
 summary(exerc1)
 summary(buyers)
-comb <- rbind(cbind(buyers, table = "Buyers"), cbind(exerc1, table="All"))
-s <- summary(table ~., data=comb, method= "reverse")
-print(s, exclude1 = TRUE)
 
+boxplotB_NB <- function(column_name){return(boxplot(c(buyers[column_name], notbuyers[column_name]), col=(c("green", "red")), main="Comparision Between Buyers and Non-Buyers", xlab=("column_name")))}
+boxplotB_NB("Age")
+
+
+  
 #Decision Tree with rpart
 #Method = "class" if you have a classification tree
 #Method = "anova" if you have a regression tree
@@ -63,7 +65,7 @@ mysolution <- data.frame(customersDB, Spcork = myprediction)
 mysolution
 
 
-#Confusing Matrix files (testing model in the 2k)
+#Confusing Matrix files (testing model in the 2k set)
 dtwines2k <- exerc1
 pred_test <- predict(dtwine_reduced, newdata = dtwines2k, type="class")
 pred_test_ds <- data.frame(ID=dtwines2k$Custid, Spcork=dtwines2k$Spcork, Pred=pred_test) 
@@ -82,16 +84,45 @@ confusionMatrix2k
 model2 <- rpart(Spcork ~ 
                           Dayswus + Income + Recency + Monetary, 
                         data = exerc1, method="class", control = rpart.control(minsplit = 10, cp = 0.002))
-#Info + viz on Decision Tree
+##Info + viz on Decision Tree
 as.party(model2)
 printcp(model2)
-#PRED
+##PRED
 dtwines_model2 <- exerc1
 pred_model2 <- predict(model2, newdata = dtwines_model2, type="class")
 pred_model2_ds <- data.frame(ID=dtwines_model2$Custid, Spcork=dtwines_model2$Spcork, Pred=pred_model2) 
-#Test
+##Test
 confusionMatrix_model2 <- confusionMatrix(pred_model2_ds$Spcork, pred_model2_ds$Pred)
 confusionMatrix_model2
+
+
+####Train and Test Set
+##Dividing the sample intro train/test set
+smp_size <- floor(0.7 * nrow(exerc1))
+set.seed(123)
+train_ind <- sample(seq_len(nrow(exerc1)), size = smp_size)
+train_wines <- exerc1[train_ind, ]
+test_wines <- exerc1[-train_ind, ]
+nrow(train_wines)+nrow(test_wines)
+nrow(train_wines)
+nrow(test_wines)
+
+#Model3 (with train_data)
+
+model_3 <- rpart(Spcork ~ Dayswus + Income + Recency + Monetary, 
+                 data = train_wines, method="class", 
+                 control = rpart.control(minsplit = 10, cp = 0.005))
+
+#Prediction
+myprediction3 <- predict(model_3, newdata = test_wines, type = "class")
+plot(myprediction3)
+
+#Solution
+mysolution3 <- data.frame(test_wines, testingspcork = myprediction3)
+
+#Confusion Matrix
+confusionMatrix3 <- confusionMatrix(mysolution3$Spcork, mysolution3$testingspcork)
+confusionMatrix3
 
 
 #Decision Tree on Predicted Data aka.. Testing solution
